0cd8dcee28fc671c178cdb7fce37292dbd57c156
ruthyi <jbruthlizcano@gmail.com>
2022-12-22 16:17:04 -0500

    prueba

--- src/components/Header/header.jsx
+++ src/components/Header/header.jsx
@@ -1,8 +1,9 @@
 import NavFirst from "../Nav/navFirst"
+
 export default function Header(){
     return(
         <div className='row d-flex justify-content-between align-self-center  div-header'>
-            <NavFirst/>
+            {/* <NavFirst/> */}
         </div>
     )
 }
